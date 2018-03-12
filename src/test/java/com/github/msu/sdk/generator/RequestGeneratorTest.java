c3b8b21e5f20f990e3f9e94a18a8b741d70a4027
isahb <isah.bllaca@asseco-see.com>
2018-03-12 17:21:06 +0100

    Generator INIT 2

--- src/test/java/com/github/msu/sdk/generator/RequestGeneratorTest.java
+++ src/test/java/com/github/msu/sdk/generator/RequestGeneratorTest.java
@@ -10,22 +10,27 @@ import com.github.msu.sdk.generator.MsuApiMetadata.Action.Params;
 
 public class RequestGeneratorTest {
 	MsuApiMetadataLoader metadataLoader;
-
+	StringCaseUtility stringCaseUtility;
+	
 	@Before
 	public void setUp() {
 		metadataLoader = new MsuApiMetadataLoader();
+		stringCaseUtility = new StringCaseUtility();
+		// TODO load template file from src/test/resources/requestTemplate.txt
 	}
 
 	@Test
 	public void testMetadataLoader() {
 		List<Action> actions = metadataLoader.getMsuApiMetadata().getAction();
 		actions.forEach(action -> {
-			System.out.println("Action: " + action.getName());
+			String actionInCamelCase = stringCaseUtility.toCamelCase(action.getName());
+			System.out.println("Action: " + actionInCamelCase);
 			Params params = action.getParams();
 			params.param.forEach(param -> {
 				System.out.println(param.getName() + ":" + param.getType());
 			});
 			System.out.println("------------------------------------------------------------");
 		});
+		// TODO
 	}
 }
---
+++ src/test/java/com/github/msu/sdk/generator/StringCaseUtility.java
@@ -0,0 +1,7 @@
+package com.github.msu.sdk.generator;
+
+public class StringCaseUtility {
+	public String toCamelCase(String input) {
+		return input.toLowerCase(); //TODO
+	}
+}
