b5966d8f00723b025f5ed4d0b47c486e2a186f02
kardelen <83505219+Kardelenyuksel@users.noreply.github.com>
2023-10-26 12:45:02 +0300

    NEW QUERYBNPLPRODUCT REQUEST IMPLEMENTED

---
+++ src/test/java/com/merchantsafeunipay/sdk/request/QueryBnplProductRequestTest.java
@@ -0,0 +1,19 @@
+package com.merchantsafeunipay.sdk.request;
+
+import com.merchantsafeunipay.sdk.request.apiv2.query.QueryBnplProductRequest;
+import com.merchantsafeunipay.sdk.response.QueryBnplProductResponse;
+import org.junit.Test;
+
+public class QueryBnplProductRequestTest extends BaseIntegrationTest {
+
+    @Test
+    public void queryBnplProduct() {
+        QueryBnplProductRequest queryBnplProductRequest = QueryBnplProductRequest.builder()
+                .withCode("1234")
+                .withValidFor("DEALER")
+                .build();
+
+        QueryBnplProductResponse queryBnplProductResponse = client.doRequest(queryBnplProductRequest);
+        System.out.println(queryBnplProductResponse.getRawResponse());
+    }
+}
